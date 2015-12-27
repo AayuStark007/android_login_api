74b0787a8d72e266353d85e31a3bba69e8bc0c00
Aayush Gupta <aayustark007@gmail.com>
2015-12-27 15:27:41 +0530

    A custom API to help android app communicate with mysql db using php

---
+++ include/DB_Functions.php
@@ -0,0 +1,96 @@
+<?php
+ 
+class DB_Functions {
+ 
+    private $conn;
+ 
+    // constructor
+    function __construct() {
+        require_once 'DB_Connect.php';
+        // connecting to database
+        $db = new DB_Connect();
+        $this->conn = $db->connect();
+    }
+ 
+    // destructor
+    function __destruct() {
+         
+    }
+ 
+    /**
+     * Storing new user
+     * returns user details
+     */
+    public function storeUser($name, $email, $password) {
+        $uuid = uniqid('', true);
+        $encrypted_password = password_hash($password, PASSWORD_BCRYPT); // encrypted password
+ 
+        $stmt = $this->conn->prepare("INSERT INTO users(unique_id, name, email, encrypted_password, created_at) VALUES(?, ?, ?, ?, NOW())");
+        $stmt->bind_param("ssss", $uuid, $name, $email, $encrypted_password);
+        $result = $stmt->execute();
+        $stmt->close();
+ 
+        // check for successful store
+        if ($result) {
+            $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = ?");
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $user = $stmt->get_result()->fetch_assoc();
+            $stmt->close();
+ 
+            return $user;
+        } else {
+            return false;
+        }
+    }
+ 
+    /**
+     * Get user by email and password
+     */
+    public function getUserByEmailAndPassword($email, $password) {
+ 
+        $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = ?");
+ 
+        $stmt->bind_param("s", $email);
+ 
+        if ($stmt->execute()) {
+            $user = $stmt->get_result()->fetch_assoc();
+            while($row = $user) {
+                $hash = $row['encrypted_password'];
+                if (password_verify($password, $hash)) {
+                    $stmt->close();
+                    return $user;
+                } else {
+                    return NULL;
+                }
+            }
+        } else {
+            return NULL;
+        }
+    }
+ 
+    /**
+     * Check user is existed or not
+     */
+    public function isUserExisted($email) {
+        $stmt = $this->conn->prepare("SELECT email from users WHERE email = ?");
+ 
+        $stmt->bind_param("s", $email);
+ 
+        $stmt->execute();
+ 
+        $stmt->store_result();
+ 
+        if ($stmt->num_rows > 0) {
+            // user existed 
+            $stmt->close();
+            return true;
+        } else {
+            // user not existed
+            $stmt->close();
+            return false;
+        }
+    }
+}
+ 
+?>
